8b7ebf5c17769682c9c378efc3dada21d3ddff17
Abdul-sid <abdulrahman.siddiqui16@gmail.com>
2019-09-24 03:42:22 -0400

    accessing within files

---
+++ File-importing/base.js
@@ -0,0 +1,5 @@
+let d = require('./source');
+
+
+console.log(d.a(12));
+console.log(d.b(12));
---
+++ File-importing/source.js
@@ -0,0 +1,15 @@
+var foo = function (age){
+   return `My age is ${age}`    
+}
+
+
+
+var bar = function (height){
+    return `My height is ${height}`    
+ }
+
+module.exports = {
+    a : foo,
+    b : bar 
+} 
+ 
