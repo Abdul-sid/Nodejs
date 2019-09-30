3f636e31b3d1533664e332a70dde9cfca60793ed
Abdul-sid <abdulrahman.siddiqui16@gmail.com>
2019-09-30 08:23:46 -0400

    delete1

--- Posts/mongo.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true , useUnifiedTopology: true});
